83665d1f66ad73be4ee10856c29d491819e881bf
benjaquintana <54126753+benjaquintana@users.noreply.github.com>
2021-10-11 07:26:16 -0500

    1

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -4,7 +4,7 @@
                 <a href="index.html"><img src="img/barra.png" alt="logo_barra"></a>
             </div>
             <div class="redes">
-            <a target="_blank" href="admin/admin_area.php" title="Administrador"><i class="fas fa-user-tie"></i></a>
+                <a target="_blank" href="admin/admin_area.php" title="Administrador"><i class="fas fa-user-tie"></i></a>
                 <a target="_blank" href="https://www.facebook.com/clubcccarg/"><i class="fab fa-facebook-square"></i></a>
                 <a target="_blank" href="https://www.youtube.com/channel/UCLNhj4X2dRFkEMrmk_nNUFA"><i class="fab fa-youtube"></i></a>
                 <a target="_blank" href="https://www.instagram.com/clubccc/?hl=es-la"><i class="fab fa-instagram"></i></a>
@@ -15,8 +15,21 @@
         </div>        
     </footer>
 
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/f5164e1266.js" crossorigin="anonymous"></script>
+    <!-- User Scripts -->
+    <script src="js/vendor/modernizr-3.11.2.min.js"></script>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+        window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
     
 </body>
 </html>
